a50d48d7e94ec2249df252d6af6c2ae75d298e04
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-02-20 13:56:08 -0300

    updating

--- fase-03/modulo04/aula/instructors.js
+++ fase-03/modulo04/aula/instructors.js
@@ -2,6 +2,11 @@ const fs = require('fs')
 const data = require('./data.json')
 const { age, date } = require('./utils')
 
+exports.index = function(req, res) {
+
+    return res.render("instructors/index", { instructors: data.instructors })
+}
+
 exports.show = function(req, res) {
     const { id } = req.params
 
@@ -69,4 +74,49 @@ exports.edit = function(req, res) {
     }
 
     return res.render('instructors/edit', { instructor }) 
+}
+
+exports.put = function(req, res) {
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex) {
+        if(id == instructor.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if(!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write file error")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
+
+exports.delete = function(req, res) {
+    const { id } = req.body
+    
+    const filteredInstructors = data.instructors.filter(function(instructor) {
+        return instructor.id != id
+    })
+
+    data.instructor = filteredInstructors
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write file error")
+
+        return res.redirect("/instructors")
+    })
 }
--- fase-03/modulo04/aula/routes.js
+++ fase-03/modulo04/aula/routes.js
@@ -6,9 +6,7 @@ routes.get('/', function(req, res) {
     return res.redirect("/instructors")
 })
 
-routes.get('/instructors', function(req, res) {
-    return res.render("instructors/index")
-})
+routes.get('/instructors', instructors.index)
 
 routes.get('/instructors/create', function(req, res) {
     return res.render("instructors/create")
@@ -19,6 +17,10 @@ routes.get('/instructors/:id', instructors.show)
 routes.get('/instructors/:id/edit', instructors.edit)
 
 routes.post("/instructors", instructors.post)
+
+routes.put("/instructors", instructors.put)
+
+routes.delete("/instructors", instructors.delete)
 
 routes.get('/members', function(req, res) {
     return res.send("members")
