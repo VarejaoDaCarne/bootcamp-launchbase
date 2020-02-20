cd77c71d1f8e14dfad4a4f12f3f11b382df75353
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-02-20 13:57:46 -0300

    updating

---
+++ fase-03/modulo04/desafio-4-4/routes.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const routes = express.Router()
+const teachers = require('./teachers')
+
+routes.get('/', function(req, res) {
+    return res.redirect("/teachers")
+})
+
+routes.get('/teachers', function(req, res) {
+    return res.render("teachers/index")
+})
+
+routes.get('/teachers/create', function(req, res) {
+    return res.render("teachers/create")
+})
+
+routes.get('/teachers/:id', teachers.show)
+
+routes.get('/teachers/:id/edit', teachers.edit)
+
+routes.post("/teachers", teachers.post)
+
+routes.put("/teachers", teachers.put)
+
+routes.delete("/teachers", teachers.delete)
+
+routes.get('/students', function(req, res) {
+    return res.send("students")
+})
+
+module.exports = routes
---
+++ fase-03/modulo04/desafio-4-5/teachers.js
@@ -0,0 +1,117 @@
+const fs = require('fs')
+const data = require('./data.json')
+const { age, date } = require('./utils')
+
+exports.show = function(req, res) {
+    const { id } = req.params
+
+    const foundTeacher = data.teachers.find(function(teacher) {
+        return id == teacher.id
+    })
+
+    if(!foundTeacher) return res.send("Teacher not found")
+
+    const teacher = {
+        ...foundTeacher,
+        age: age(foundTeacher.birth),
+        occupations: foundTeacher.occupations.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundTeacher.created_at),
+    }
+    return res.render("teachers/show", { teacher: teacher })
+}
+
+exports.post = function(req, res) {
+    const keys = Object.keys(req.body)
+
+    for(key of keys) {
+       if(req.body[key] == "")  {
+           return res.send('Please, fill all fields')
+       }
+    }
+
+    let {avatar_url, name, birth, graduation, type, occupations} = req.body
+
+    req.body.birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.teachers.length + 1)
+
+    data.teachers.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        graduation,
+        type,
+        occupations,
+        created_at
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write file error")
+
+        return res.redirect("/teachers")
+    })
+
+    // return res.send(req.body)
+}
+
+exports.edit = function(req, res) {
+    const { id } = req.params
+
+    const foundTeacher = data.teachers.find(function(teacher) {
+        return id == teacher.id
+    })
+
+    if(!foundTeacher) return res.send("Teacher not found")
+
+    const teacher = {
+        ...foundTeacher,
+        birth: date(foundTeacher.birth),
+    }
+
+    return res.render('teachers/edit', { teacher }) 
+}
+
+exports.put = function(req, res) {
+    const { id } = req.body
+    let index = 0
+
+    const foundTeacher = data.teachers.find(function(teacher, foundIndex) {
+        if(id == teacher.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if(!foundTeacher) return res.send("Teacher not found")
+
+    const teacher = {
+        ...foundTeacher,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.teachers[index] = teacher
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write file error")
+
+        return res.redirect(`/teachers/${id}`)
+    })
+}
+
+exports.delete = function(req, res) {
+    const { id } = req.body
+    
+    const filteredTeachers = data.teachers.filter(function(teacher) {
+        return teacher.id != id
+    })
+
+    data.teacher = filteredTeachers
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write file error")
+
+        return res.redirect("/teachers")
+    })
+}
